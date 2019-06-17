da633ea4f85a143d70265620e727b34435914800
Piyush <bhangalepiyush@gmail.com>
2019-06-17 19:38:41 +0530

    v 0.1.0

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @author Piyush Bhangale <bhangalepiyush@gmail.com>
+ * @license GNU GPLv3
+ * @file Export all classes of the package
+ */
+
+export = {
+    /**
+     * @since 0.1.0
+     * @description What version the package is on
+     */
+    version: require("../package.json").version,
+};
